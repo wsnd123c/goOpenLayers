eb66ef8b4fca798ace320320f64dd88581911d61
wsnd123c <115916455+wsnd123c@users.noreply.github.com>
2025-09-22 18:11:53 +0800

    删除错误的absolute路径文件

--- absolute/path/to/cmd/tegola/cmd/server.go
+++
@@ -1,8 +0,0 @@
-// ... existing code ...
-if len(conf.AppConfigSource) > 0 {
-    log.Info("Initializing app config source...")
-    configWatcher = initConfigSource(context.Background()) // 修改这里
-} else {
-    log.Info("No app config source configured")
-}
-// ... existing code ...
